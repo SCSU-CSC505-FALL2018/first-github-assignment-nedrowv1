281f85f4d98b9fb67bcb16df13b270e45c3d8663
nedrowv1 <42817020+nedrowv1@users.noreply.github.com>
2018-09-16 08:31:42 -0400

    Initial Build
    binary converter module with master_convert user interface

---
+++ master_convert.py
@@ -0,0 +1,27 @@
+"""function switcher.  dec->bin; bin->dec; eventually dec->hex; hex->dec
+gather user input"""
+
+import binary_converter
+
+CONTINUE = True
+print("Number Conversion Utility")
+while CONTINUE:
+
+
+    print("MENU:".center(23))
+    print("1 - Decimal to Binary\n2 - Binary to Decimal")
+    CHOICE = input("Option:_")
+    if CHOICE == "1":
+        YOUR_NUMBER = float(input("Enter a Decimal:_"))
+        YOUR_OUTPUT = binary_converter.convert_dec_to_bin(YOUR_NUMBER)
+        YOUR_TYPE = "binary"
+    elif CHOICE == "2":
+        YOUR_NUMBER = float(input("Enter a Binary:_"))
+        YOUR_OUTPUT = binary_converter.convert_bin_to_dec(YOUR_NUMBER)
+        YOUR_TYPE = "decimal"
+    else:
+        pass
+
+    print(YOUR_NUMBER, " in ", YOUR_TYPE, " is ", YOUR_OUTPUT)
+    if input("Continue: [Y]es/[N]o ").casefold() == "N".casefold():
+        CONTINUE = False
